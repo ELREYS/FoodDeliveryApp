a866ad9cd80cf303fd3c9086677f6ee154a29e31
ELREYS <gdilisa@hotmail.com>
2020-04-15 20:18:52 +0200

    Initial Commit

---
+++ FoodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by MMBBS0000 on 15.04.20.
+//  Copyright © 2020 gditdev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
